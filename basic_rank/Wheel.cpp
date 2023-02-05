e070c9fbf9588723bc48b55fc110aadb7680bbe7
Naimul Hoque <nhoque@irobot.com>
2023-02-05 14:17:52 -0800

    NH: Added implementation for member functions

--- basic_rank/Wheel.cpp
+++ basic_rank/Wheel.cpp
@@ -1,7 +1,37 @@
 #include "Arduino.h"
 #include "Wheel.h"
 
-Wheel::Wheel(int pin_number){
-    pin = pin_number;
-    pinMode(pin, INPUT);
+Wheel::Wheel(m_pin, p_pin){
+  motor_pin = m_pin;
+  pwm_pin = p_pin;
+  pinMode(motor_pin, OUTPUT);
+  pinMode(pwm_pin, OUTPUT);
+}
+
+void Wheel::set_motor_pin(int m_pin){
+  motor_pin = m_pin;
+  pinMode(motor_pin, OUTPUT);
+}
+
+void Wheel::set_pwm_pin(int p_pin){
+  pwm_pin = p_pin;
+  pinMode(pwm_pin, OUTPUT);
+}
+
+void Wheel::rotate_clockwise(int speed){
+  digitalWrite(motor_pin, HIGH);
+  analogWrite(pwm_pin, speed);
+}
+
+void Wheel::rotate_counterclockwise(int speed){
+  digitalWrite(motor_pin, LOW);
+  analogWrite(pwm_pin, speed);
+}
+
+void Wheel::set_speed(int speed){
+  analogWrite(pwm_pin, speed);
+}
+
+void Wheel::stop(){
+  set_speed(0);
 }
